e249ad7e6bf258ac8cb2c2d464bf07324bb180b3
qays-test <qaysalnajjad317@gmail.com>
2025-12-18 11:52:59 +0400

    Update outputs.tf

--- stages/foundation/outputs.tf
+++ stages/foundation/outputs.tf
@@ -5,7 +5,3 @@ output "vpc_id" {
 output "ecs_subnets_ids" {
     value = module.foundation.ecs_subnets_ids
 }
-  
-output "vpc_endpoint_sg_id" {
-    value = module.foundation.vpc_endpoint_sg_id
-}
